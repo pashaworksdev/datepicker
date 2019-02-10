62e9cc0aab84e5208be9e1f572360e16eb6c5351
pauldiss <pushkarev.p.m@gmail.com>
2019-02-10 13:16:30 +0300

    Commit #2 from 09.02.2019

--- datepicker.js
+++ datepicker.js
@@ -4,101 +4,155 @@ class Datepicker {
 
     constructor(containerGeneral) {
         this.containerGeneral = containerGeneral;
+        this.containerGeneral.setAttribute("class", "datepicker");
         this.arrowLeft = new ArrowLeft();
         this.month = new Month();
         this.arrowRight = new ArrowRight();
-        this.date = new Date();
-        // this.containerDate = new ContainerDate();
+        this.containerWeek = new ContainerWeek();
+        this.containerMonth = new ContainerMonth();
     }
 
     draw() {
         this.containerGeneral.appendChild(this.arrowLeft.draw());
         this.containerGeneral.appendChild(this.month.draw());
         this.containerGeneral.appendChild(this.arrowRight.draw());
-        // this.containerGeneral.appendChild(this.containerDate.draw());
-    }
-
-    // getDateNow() {
-    //     this.date = new Date();
-    //     return this.date;
-    // }
-
-    setDateCurrent(change) {
-        if (change == 'left') {
-            this.date.setMonth(this.date.getMonth() - 1);
-            this.month.writeNameMonth();
-        } else {
-            this.date.setMonth(this.date.getMonth() + 1);
-            this.month.writeNameMonth();
-        }
-
+        this.containerGeneral.appendChild(this.containerWeek.draw());
+        this.containerGeneral.appendChild(this.containerMonth.draw());
     }
 
 }
 
 class Month {
 
-    draw() {
+    constructor() {
+        this.date = new Date();
         this.containerMonth = document.createElement('div');
         this.containerMonth.setAttribute("class", "datepicker__month");
-        this.writeNameMonth();
+    }
+
+    getDateCurrent() {
+        return this.date;
+    }
+
+    setDateCurrent(change) {
+        if (change === 'left') {
+            this.date.setMonth(this.date.getMonth() - 1);
+        } else {
+            this.date.setMonth(this.date.getMonth() + 1);
+        }
+        this.writeMonthName();
+    }
+
+    draw() {
+        this.writeMonthName();
         return this.containerMonth;
     }
 
-    writeNameMonth() {
+    writeMonthName() {
         let months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
-        this.containerMonth.innerHTML = months[datepicker.date.getMonth().toString()];
+        this.containerMonth.innerHTML = months[this.getDateCurrent().getMonth().toString()] + ' ' + this.getDateCurrent().getFullYear();
     }
-
-    // update() {
-    //     let months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
-    //     this.containerMonth.innerHTML = months[datepicker.date.getMonth().toString()];
-    // }
 
 }
 
 class Arrow {
 
-    draw() {
-        this.containerArrow = document.createElement('div');
-        this.containerArrow.setAttribute("class", "datepicker__button");
+    constructor() {
         this.buttonArrow = document.createElement('div');
         this.buttonArrow.setAttribute("class", "datepicker__arrow");
-        this.containerArrow.appendChild(this.buttonArrow);
     }
 
 }
-
+//обращаюсь к методу click через datepicker.month - попытаться это исправить
 class ArrowLeft extends Arrow {
 
     draw() {
-        super.draw();
         this.buttonArrow.classList.add("datepicker__arrow_direction_left");
         this.buttonArrow.onclick = () => this.click();
-        return this.containerArrow;
+        return this.buttonArrow;
     }
 
     click() {
-        // alert('TEST');
-        datepicker.setDateCurrent('left');
-        // alert(datepicker.setDateCurrent());
+        datepicker.month.setDateCurrent('left');
     }
 
 }
-
+//обращаюсь к методу click через datepicker.month - попытаться это исправить
 class ArrowRight extends Arrow {
 
     draw() {
-        super.draw();
         this.buttonArrow.classList.add("datepicker__arrow_direction_right");
         this.buttonArrow.onclick = () => this.click();
-        return this.containerArrow;
+        return this.buttonArrow;
     }
 
     click() {
-        // alert('TEST');
-        datepicker.setDateCurrent('right');
-        // alert(datepicker.setDateCurrent());
+        datepicker.month.setDateCurrent('right');
+    }
+
+}
+// возможно можно сделать дочерним классом
+class ContainerWeek {
+
+    constructor() {
+        this.containerWeek = document.createElement('div');
+        this.containerWeek.setAttribute("class", "datepicker__containerWeek");
+    }
+
+    draw() {
+        let week = ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"];
+        for (var day = 0; day < 7; day++) {
+            this.createDay();
+            this.dayWeek.innerHTML = week[day];
+        }
+        return this.containerWeek;
+    }
+
+    createDay() {
+        this.dayWeek = document.createElement('div');
+        this.dayWeek.setAttribute("class", "containerWeek");
+        this.dayWeek.classList.add("containerWeek__dayWeek");
+        this.containerWeek.appendChild(this.dayWeek);
+    }
+
+}
+// возможно можно сделать дочерним классом
+class ContainerMonth {
+
+    constructor() {
+        this.containerMonth = document.createElement('div');
+        this.containerMonth.setAttribute("class", "datepicker__containerMonth");
+    }
+
+    draw() {
+        for (var day = 0; day < 2; day++) {
+            this.createDay();
+            this.dayNumber.innerHTML = (() => this.startDate())();
+        }
+        return this.containerMonth;
+    }
+
+    createDay() {
+        this.dayNumber = document.createElement('div');
+        this.dayNumber.setAttribute("class", "containerMonth");
+        this.dayNumber.classList.add("containerMonth__dayNumber");
+        this.containerMonth.appendChild(this.dayNumber);
+    }
+
+    startDate() {
+        let currentMonth = datepicker.month.getDateCurrent();
+        currentMonth.setDate(1);
+        //
+        // let firstDay = new Date(currentMonth);
+        // // alert(firstDay);
+        //
+        // let dayWeek = currentMonth.getDay();
+        // // alert(dayWeek);
+        //
+        // alert(firstDay.getDate() - dayWeek);
+
+        let day = currentMonth.getDate(new Date(currentMonth.setDate(2 - currentMonth.getDay())));
+        return day;
     }
 
 }
